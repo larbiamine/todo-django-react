39f8a901f373cbd1149e87913aab964062c2f126
larbiamine <larbiaminelj@gmail.com>
2022-10-19 17:38:54 +0100

    fixed bd

--- crudjangoapi/wsgi.py
+++ crudjangoapi/wsgi.py
@@ -10,12 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import DjangoWhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudjangoapi.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-
-
 
 
